bd7d6882b3ff3f43fe5363fddc5da3a00f44c513
Jarosław Gorzko <gorzkojaroslaw@gmail.com>
2020-11-03 13:16:45 +0100

    first test

---
+++ src/main/java/pl/sdk/Board.java
@@ -0,0 +1,20 @@
+package pl.sdk;
+
+import java.util.HashMap;
+
+class Board {
+    private final Map<Point, Creature> map;
+
+    Board() {
+        map = new HashMap<>();
+    }
+
+    void add(Point aPoint, Creature aCreature) {
+        map.put(aPoint, aCreature);
+    }
+
+    Creature get(int aX, int aY) {
+        return map.get(new Point(aX, aY));
+
+    }
+}
--- src/main/java/pl/sdk/Point.java
+++ src/main/java/pl/sdk/Point.java
@@ -1,65 +1,14 @@
 package pl.sdk;
 
-import java.util.Objects;
+class Point {
 
-public class Point {
-    private double distanceToCenter;
-    private int x;
-    private int y;
+    final private int x;
+    final private int y;
 
-    public Point(int aX, int aY) {
-        this.x = aX;
-        this.y = aY;
-       distanceToCenter =  Math.sqrt(x*x+y*y);
-    }
-    public Point(Point aPoint1){
-        this(aPoint1.x,aPoint1.y);
+     Point(int aX, int aY) {
+        x = aX;
+        y = aY;
     }
 
-    public int getX() {
-        return x;
-    }
 
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    @Override
-    public String toString() {
-        return "Point{" +
-                "x=" + x +
-                ", y=" + y +
-                '}';
-    }
-
-    @Override
-    public Object clone() throws CloneNotSupportedException {
-        return new Point(x, y);
-    }
-
-    public double getDistanceToCenter() {
-        return getDistanceToCenter();
-    }
-
-    @Override
-    public boolean equals(Object aO) {
-        if (this == aO) return true;
-        if (aO == null || getClass() != aO.getClass()) return false;
-        Point point = (Point) aO;
-        return x == point.x &&
-                y == point.y;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(x, y);
-    }
 }
