1cb2eb363756b0beb30fa58bac275c468ab57f96
Jarosław Gorzko <gorzkojaroslaw@gmail.com>
2020-11-05 14:05:46 +0100

    add creature to not empty field test

--- src/test/java/pl/sdk/BoardTest.java
+++ src/test/java/pl/sdk/BoardTest.java
@@ -1,11 +1,20 @@
 package pl.sdk;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
+private Board board;
+private Creature creature;
 
+    @BeforeEach
+    void init(){
+
+        Board board = new Board();
+        Creature creature = new Creature();
+    }
     @Test
     void shouldAddCreature() {
         Board board = new Board();
@@ -17,6 +26,22 @@ class BoardTest {
         assertEquals(creature, creatureFromBoard);
 
     }
+@Test
+    void shouldRedturnNullWhenFiledIsEmpty(){
+        Creature creatureFromBoard = board.get(0, 0);
 
+        assertNull(creatureFromBoard);
+
+}
+@Test
+    void shouldXWhenYouTryAddCreatureToNotEmptyField(){
+        board.add(new Point(0, 0),creature);
+        Creature creature2 = new Creature();
+        board.add(new Point(0, 0),creature);
+
+        Creature creatureFromBoard = board.get(0, 0);
+
+        assertEquals(creature, creatureFromBoard);
+}
 
 }
