ba65cb21b2d22689e013c9855139a5740ec60ca6
Jarosław Gorzko <gorzkojaroslaw@gmail.com>
2020-11-11 10:48:23 +0100

    creature move test

--- src/main/java/pl/sdk/Board.java
+++ src/main/java/pl/sdk/Board.java
@@ -11,13 +11,19 @@ class Board {
     }
 
     void add(Point aPoint, Creature aCreature) {
-      if(map.containsKey(aPoint)){
-          throw new IllegalArgumentException();
-      }
-      map.put(aPoint, aCreature);
+        if (map.containsKey(aPoint)) {
+            throw new IllegalArgumentException();
+        }
+        map.put(aPoint, aCreature);
     }
 
     Creature get(int aX, int aY) {
         return map.get(new Point(aX, aY));
+    }
+
+    void move(Point aSourcePoint, Point aTargetPoint1) {
+        Creature creatureFromSourcePoint = map.get(aSourcePoint);
+        map.remove(aSourcePoint);
+        map.put(aTargetPoint1, creatureFromSourcePoint);
     }
 }
--- src/test/java/pl/sdk/BoardMovingTest.java
+++ src/test/java/pl/sdk/BoardMovingTest.java
@@ -16,7 +16,17 @@ class BoardMovingTest {
         board = new Board();
         creature = new Creature();
         creature2 = new Creature();
-        board.add(new Point(0, 0),creature);
+        board.add(new Point(0, 0), creature);
     }
+
     @Test
+    void creatureShouldMoveCorrectly() {
+        board.move(new Point(0, 0), new Point(0, 1));
+
+        Creature creatureFromBoard = board.get(0, 1);
+
+        assertEquals(creature, creatureFromBoard);
+        assertNull(board.get(0, 0));
+    }
+
 }
