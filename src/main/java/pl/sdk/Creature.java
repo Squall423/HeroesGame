c8b9147729d6acd07c123d7789f2123ce5c47ba8
Jarosław Gorzko <gorzkojaroslaw@gmail.com>
2020-11-13 11:00:37 +0100

    creature statistic

--- src/main/java/pl/sdk/Creature.java
+++ src/main/java/pl/sdk/Creature.java
@@ -2,5 +2,14 @@ package pl.sdk;
 
 class Creature {
 
+    private CreatureStatistic stats;
+    private int currentHp;
+    Creature() {
+        stats = new CreatureStatistic("DefName", 1, 100, 1, 1);
+    }
+
+    Creature(String aName, int aAttack, int aArmor, int aMaxHp, int aMoveRange) {
+        stats = new CreatureStatistic(aName, aAttack, aArmor, aMaxHp, aMoveRange);
+    }
 
 }
