d00026e30ee3459dbd6f4c623f5e46a3b5455994
Jarosław Gorzko <gorzkojaroslaw@gmail.com>
2020-11-14 12:51:03 +0100

    counterAttacking test and impl

--- src/main/java/pl/sdk/Creature.java
+++ src/main/java/pl/sdk/Creature.java
@@ -4,6 +4,7 @@ class Creature {
 
     private CreatureStatistic stats;
     private int currentHp;
+    private boolean counterAttackedInThisTurn;
 
     Creature() {
         stats = new CreatureStatistic("DefName", 1, 100, 1, 1);
@@ -15,18 +16,30 @@ class Creature {
     }
 
     void attack(Creature aDefender) {
-        if(isAlive()) {
-            int damageToDeal = this.stats.getAttack() - aDefender.stats.getArmor();
-            if (damageToDeal < 0) {
-                damageToDeal = 0;
-            }
+        if (isAlive()) {
+            int damageToDeal = calculateDamage(aDefender);
             aDefender.currentHp = aDefender.currentHp - damageToDeal;
+
+            if (!aDefender.counterAttackedInThisTurn) {
+                int damageToDealInCounterAttack = aDefender.calculateDamage(this);
+                currentHp = currentHp - damageToDealInCounterAttack;
+               aDefender.counterAttackedInThisTurn = true;
+            }
         }
+    }
+
+    private int calculateDamage(Creature aDefender) {
+        int damageToDeal = this.stats.getAttack() - aDefender.stats.getArmor();
+        if (damageToDeal < 0) {
+            damageToDeal = 0;
+        }
+        return damageToDeal;
     }
 
     private boolean isAlive() {
         return currentHp > 0;
     }
+
     int getCurrentHp() {
         return currentHp;
     }
--- src/test/java/pl/sdk/CreateCounterAttackTest.java
+++ src/test/java/pl/sdk/CreateCounterAttackTest.java
@@ -1,6 +1,5 @@
 package pl.sdk;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -18,5 +17,16 @@ public class CreateCounterAttackTest {
 
         assertEquals(90, attacker.getCurrentHp());
 
+    }
+    @Test
+    void creatureShouldCounterAttackOnlyOnceAtTurn(){
+        Creature attacker = new Creature("Attacker",NOT_IMPORTANT,10,100,NOT_IMPORTANT);
+        Creature attacker2 = new Creature("Attacker",NOT_IMPORTANT,10,100,NOT_IMPORTANT);
+        Creature defender = new Creature("Defender",20,NOT_IMPORTANT,100,NOT_IMPORTANT);
+
+        attacker.attack(defender);
+
+        assertEquals(90, attacker.getCurrentHp());
+        assertEquals(100, attacker2.getCurrentHp());
     }
 }
